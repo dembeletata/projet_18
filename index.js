fa87915f1f0cdea0d690893e7ba6e910faa95107
dembele tata <dembeletata138@gmail.com>
2023-09-04 00:56:30 +0000

    initialisation

---
+++ index.js
@@ -0,0 +1,10 @@
+window.open(
+    document.body.innerHTML += `<nav style=" background: rgb(80, 80, 80); width: 100%; height: 50px;">
+    <ul style=" display: flex; flex-direction: row; justify-content: space-between; align-items: center;
+    padding-left: 20%; padding-right: 20%; padding-top: 10px; font-size: 20px; color: white; flex-wrap: wrap; background: rgb(80, 80, 80);">
+        <a>accueil</a>
+        <a>service</a>
+        <a>conttact</a>
+    </ul>
+</nav>`
+);
